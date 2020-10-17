121f1ca8bbd44b1461059603aa2d886b96cdbd5f
IvanPostu <ipostu20000127@gmail.com>
2020-10-17 20:25:28 +0300

    Routes are extracted into a separate component.

--- App.tsx
+++ App.tsx
@@ -1,33 +1,11 @@
 import React, { ReactElement, FC } from 'react'
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
-
-import { routes } from '@/routes/routes'
-import { Header } from '@/components/header'
 import { ReduxWrapper } from '@/store/root/ReduxWrapper'
-
-const Stack = createStackNavigator()
+import ApplicationRouterWrapper from '@/routes/AppRouter'
 
 const App: FC = (): ReactElement => {
   return (
     <ReduxWrapper>
-      <NavigationContainer>
-        <Stack.Navigator>
-          {Object.keys(routes).map((key) => (
-            <Stack.Screen
-              key={key}
-              name={routes[key].routeName}
-              component={routes[key].component}
-              options={({ navigation }) => ({
-                // eslint-disable-next-line react/display-name
-                header: (props) => <Header {...props} />,
-                animationEnabled: true,
-                navigation,
-              })}
-            />
-          ))}
-        </Stack.Navigator>
-      </NavigationContainer>
+      <ApplicationRouterWrapper />
     </ReduxWrapper>
   )
 }
